492ded069f499ee5ed34ac5ac273df6b78d16bc6
Andrew-Thomas-ZR <zackrevan@gmail.com>
2025-02-16 22:36:31 -0600

    Add files via upload

---
+++ Estatica.py
@@ -0,0 +1,17 @@
+# Memoria Estatica en Python
+calificaciones = [0] * 5  
+
+for i in range(5):
+    calificaciones[i] = int(input(f"Captura la calificación {i + 1}: "))
+
+print("Calificaciones:", calificaciones)
+
+# Memoria Dinamica en Python
+frutas = ["Mango", "Manzana", "Banana", "Uvas"]
+print(frutas)
+
+frutas.pop(0)  
+frutas.pop(1)  
+
+frutas.append("Sandía")
+print(frutas)
